e131c44d07e9006a72b79a9205a320a6d348ee4a
Erick Stopassoli <77657387+estopassoli@users.noreply.github.com>
2022-02-02 10:11:28 -0300

    Add files via upload

--- chatlol-api.sql
+++ chatlol-api.sql
@@ -10,90 +10,46 @@ Target Server Type    : MYSQL
 Target Server Version : 80017
 File Encoding         : 65001
 
-Date: 2022-02-01 05:47:06
-*/
-
-/* 
-mysql -uroot -h127.0.0.1 -p12345 < chatlol-api.sql
-mysql -uadmin -h127.0.0.1 -p123 summoners
+Date: 2022-02-02 09:35:33
 */
 
 SET FOREIGN_KEY_CHECKS=0;
+-- ----------------------------
+-- Table structure for `smurfs`
+-- ----------------------------
+DROP TABLE IF EXISTS `smurfs`;
+CREATE TABLE `smurfs` (
+  `summonerid` int(32) NOT NULL,
+  `chatid` varchar(0) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci DEFAULT NULL,
+  `nickname` varchar(0) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci DEFAULT NULL,
+  `level` int(32) DEFAULT NULL,
+  `elosoloq` varchar(0) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci DEFAULT NULL,
+  `winrate` varchar(0) DEFAULT NULL,
+  PRIMARY KEY (`summonerid`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 -- ----------------------------
---     Database creation     --
+-- Records of smurfs
 -- ----------------------------
 
-DROP DATABASE IF EXISTS `summoners`;
-CREATE DATABASE `summoners`;
-USE `summoners`;
-
--- -------------------------------- --
--- Table structure for `summoners`  --
--- -------------------------------- --
-
+-- ----------------------------
+-- Table structure for `summoner`
+-- ----------------------------
 DROP TABLE IF EXISTS `summoner`;
 CREATE TABLE `summoner` (
-  `id` int(30) AUTO_INCREMENT,
-  `summonerid` varchar(100) UNIQUE,
+  `summonerid` int(8) NOT NULL AUTO_INCREMENT,
   `chatid` varchar(32) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci DEFAULT NULL,
   `realname` varchar(32) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci DEFAULT NULL,
   `nickname` varchar(16) DEFAULT NULL,
+  `elosoloq` varchar(32) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci DEFAULT NULL,
+  `winrate` varchar(32) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci DEFAULT NULL,
+  `role` varchar(16) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci DEFAULT NULL,
   `level` int(16) DEFAULT NULL,
   `stage` varchar(32) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+  PRIMARY KEY (`summonerid`)
+) ENGINE=InnoDB AUTO_INCREMENT=15 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
-create user IF NOT EXISTS 'admin' IDENTIFIED BY '123';
-grant all privileges on summoners.* to admin;
-
--- ----------------------------------- --
--- Table structure for `summonerDataSoloQ`  --
--- ----------------------------------- --
-
-DROP TABLE IF EXISTS `summonerDataRANKED_SOLO_5x5`;
-CREATE TABLE `summonerDataRANKED_SOLO_5x5` (
-  `id` int(30) AUTO_INCREMENT,
-  `summonerId` varchar(100) UNIQUE,
-  `queueType` varchar(30),
-  `tier` varchar(21),
-  `rank` varchar(5),
-  `leaguePoints` int(4),
-  `wins` int(11),
-  `losses` int(11),
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-
--- ----------------------------------- --
--- Table structure for `summonerDataFlex`  --
--- ----------------------------------- --
-
-DROP TABLE IF EXISTS `summonerDataRANKED_FLEX_5x5`;
-CREATE TABLE `summonerDataRANKED_FLEX_5x5` (
-  `id` int(30) AUTO_INCREMENT,
-  `summonerId` varchar(100) UNIQUE,
-  `queueType` varchar(30),
-  `tier` varchar(21),
-  `rank` varchar(5),
-  `leaguePoints` int(4),
-  `wins` int(11),
-  `losses` int(11),
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-
--- ----------------------------------- --
--- Table structure for `summonerDataRANKED_TFT_PAIRS`  --
--- ----------------------------------- --
-
-DROP TABLE IF EXISTS `summonerDataRANKED_TFT_PAIRS`;
-CREATE TABLE `summonerDataRANKED_TFT_PAIRS` (
-  `id` int(30) AUTO_INCREMENT,
-  `summonerId` varchar(100) UNIQUE,
-  `queueType` varchar(30),
-  `tier` varchar(21),
-  `rank` varchar(5),
-  `leaguePoints` int(4),
-  `wins` int(11),
-  `losses` int(11),
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+-- ----------------------------
+-- Records of summoner
+-- ----------------------------
+INSERT INTO `summoner` VALUES ('14', '5513996597134@c.us', 'Riquie', 'Riquie', 'GOLDII', '38.10%', null, '250', 'comandos');
